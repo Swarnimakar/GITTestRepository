5c76825032ffcb7a0a3f9d76bb51512cfe8ead98
Vaijinath <STAR@Swarnima-QA-PC>
2016-05-09 16:47:58 +0530

    Swarnima has updated the proj

---
+++ GITTestProject/src/com/ascent/GITFirstExample.java
@@ -0,0 +1,11 @@
+package com.ascent;
+
+public class GITFirstExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellllooo");
+
+	}
+
+}
